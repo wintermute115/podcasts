2b98baac12b26f5537d088a01e29e955828ebce4
ross-thompson <ross.thompson@tritondigital.com>
2016-11-19 08:28:50 -0500

    Added function to create table if it doesn't exist; allowed for custom tablename

--- connect.pl
+++ connect.pl
@@ -61,7 +61,10 @@ sub add_podcast {
 	my $toggle = $_[3];
 	$toggle = ($toggle eq "on" ? "0" : "1");
 
-	my $sql = "INSERT INTO podcasts (podcast_name, podcast_feed, podcast_skip, podcast_last_downloaded) ";
+	my $sql = "CREATE TABLE IF NOT EXISTS `" . $DB::tablename . "` (`podcast_id` SMALLINT(5) NOT NULL AUTO_INCREMENT, `podcast_name` VARCHAR(50) NOT NULL, `podcast_feed` VARCHAR(100) NOT NULL, `podcast_skip` ENUM('0','1') NOT NULL DEFAULT '0', `podcast_last_downloaded` DATETIME NOT NULL, PRIMARY KEY (`podcast_id`) ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;";
+	$conn->query($sql);
+
+	$sql = "INSERT INTO podcasts (podcast_name, podcast_feed, podcast_skip, podcast_last_downloaded) ";
 	$sql   .= "VALUES ('$name', '$url', '$toggle', '2000-01-01 00:00:00')";
 	$conn->query($sql);
 	return $conn->create_record_iterator;
@@ -72,7 +75,7 @@ sub get_podcast_list
 	my $conn = $_[0];
 	my $podcast = $_[1];
 	# Set up the MySQL query
-	my $sql = "SELECT podcast_id, podcast_name, podcast_feed, podcast_last_downloaded FROM podcasts ";
+	my $sql = "SELECT podcast_id, podcast_name, podcast_feed, podcast_last_downloaded FROM " . $DB::tablename;
 	if ($podcast eq "")
 	{
 		$sql .= "WHERE podcast_skip = '0' ORDER BY podcast_name ASC";
@@ -97,8 +100,9 @@ sub get_podcast_rows
 	my $conn = $_[0];
 	my $order = $_[1];
 	$order = ($order eq "date" ? "podcast_last_downloaded DESC" : "podcast_name ASC");
-	my $sql = "SELECT MAX(CHAR_LENGTH(podcast_id)) AS podcast_id, MAX(CHAR_LENGTH(podcast_name)) AS podcast_name, 1 AS podcast_skip, 18 AS podcast_last_downloaded, 1 AS sort FROM podcasts ";
-	$sql .= "UNION SELECT podcast_id, podcast_name, podcast_skip, podcast_last_downloaded, 2 FROM podcasts order by sort ASC, " . $order;
+	my $sql = "SELECT MAX(CHAR_LENGTH(podcast_id)) AS podcast_id, MAX(CHAR_LENGTH(podcast_name)) AS podcast_name, 1 AS podcast_skip, 18 AS podcast_last_downloaded, 1 AS sort FROM " . $DB::tablename;
+	$sql .= " UNION SELECT podcast_id, podcast_name, podcast_skip, podcast_last_downloaded, 2 FROM " . $DB::tablename . " order by sort ASC, " . $order;
+	print $sql . "\n";
 	$conn->query($sql);
 	return $conn->create_record_iterator;
 }
@@ -108,7 +112,7 @@ sub write_to_database
 	my $conn = $_[0];
 	my $id = $_[1];
 	my $new_date = $_[2];
-	$conn->query("UPDATE podcasts SET podcast_last_downloaded='$new_date' WHERE podcast_id=$id");
+	$conn->query("UPDATE " . $DB::tablename . " SET podcast_last_downloaded='$new_date' WHERE podcast_id=$id");
 }
 
 sub close_connection
@@ -120,7 +124,7 @@ sub close_connection
 sub dump_database
 {
 	#Back up db to the cloud
-	system("mysqldump -upodcasts -ppodpass podcasts > '/home/ross/SpiderOak Hive/sql/podcasts.sql'");
+	system("mysqldump -upodcasts -ppodpass " . $DB::tablename . " > '/home/ross/SpiderOak Hive/sql/podcasts.sql'");
 }
 
 1;
