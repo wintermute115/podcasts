0f1a37988e26724c6c74fd8ee5e16f50ef9cf723
Ross Thompson <github@wintermute115.fastmail.com>
2024-03-26 18:16:15 -0400

    Adding backup

---
+++ backup.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Copy qw(cp);
+use File::Rsync;
+
+my $target = "/home/ross/Documents/ipod/";
+
+sub copy_dir
+{
+	my $source = $_[0];
+	my $dest = $target . $_[1];
+	my $delete = $_[2];
+
+	my $sync = File::Rsync->new(src=>$source, dest=>$dest, delete=>$delete, archive=>1);
+
+	$sync->exec();
+
+
+}
+
+sub copy_file
+{
+	my $source = $_[0];
+	my $dest = $target . $_[1];
+	cp($source, $dest);
+
+}
